030b7645813e6dc0585a2f51fb03eaad5a21900f
Gustavo Garcia <gustavogb@gmail.com>
2023-03-14 19:50:44 +0100

    Remove duplicated log levels

--- logging.js
+++ logging.js
@@ -4,13 +4,9 @@ const { DEBUG } = process.env;
 
 const { combine, splat, timestamp, json } = winston.format;
 
-const addSeverity = winston.format(logEntry => {
-    return { severity: logEntry.level.toUpperCase(), ...logEntry };
-});
-
 const loggerOptions = {
     level: DEBUG ? 'debug' : 'info',
-    format: combine(timestamp(), splat(), addSeverity(), json()),
+    format: combine(timestamp(), splat(), json()),
     transports: [new winston.transports.Console()],
 };
 
